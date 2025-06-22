e74cb7b48c9c140f9ee81cedafaa326a5ccd0b56
Clément Jonckheere <clem.jonckheere@gmail.com>
2025-06-22 15:45:11 +0200

    Initial commit with full structure

---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from api.v1 import users, recipes
+
+app = FastAPI(title="Healthy Recipe API")
+
+app.include_router(users.router, prefix="/api/v1/users", tags=["Users"])
+app.include_router(recipes.router, prefix="/api/v1/recipes", tags=["Recipes"])
