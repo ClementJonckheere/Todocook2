807b4f71312bef0275f69f42b247a4bf02b22706
Clément Jonckheere <clem.jonckheere@gmail.com>
2025-06-22 15:48:39 +0200

    Initialisation

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ from api.v1 import users, recipes
 app = FastAPI(title="Healthy Recipe API")
 
 app.include_router(users.router, prefix="/api/v1/users", tags=["Users"])
+
+
+
 app.include_router(recipes.router, prefix="/api/v1/recipes", tags=["Recipes"])
+ # initialisation projet
+
